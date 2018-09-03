42cddc241b89cc5cd9ffc65e4b1b5031a0bacac5
Chase Hardin <chase.hardin@scribbletechnology.com>
2018-09-03 11:58:02 -0500

    Show movie title on page

--- src/App/App.js
+++ src/App/App.js
@@ -3,17 +3,22 @@ import logo from './../logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    const movie = {
+      title: 'The Lord of the Rings: The Fellowship of the Ring'
+    }
+
+    this.state = {
+      movie: movie
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="movie-title">{this.state.movie.title}</div>
     );
   }
 }
--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure, shallow, mount } from 'enzyme';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+var assert = require('assert');
+
+configure({ adapter: new Adapter() });
+
+describe('Movies', () => {
+  it('should show a movie title', () => {
+    const wrapper = mount(<App />);
+
+    var actual = wrapper.find('.movie-title').text();
+    var expected = 'The Lord of the Rings: The Fellowship of the Ring';
+
+    assert.equal(actual, expected);
+  });
 });
